37cad276b30faafe574e6395e70291166118f86d
Roy <yyss9103@gmail.com>
2023-10-20 23:09:54 +0900

    Header compnent test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 
+function Header() {
+  return <>
+    <h1>My shoping Mall</h1>
+  </>
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello word</h1>
+      <Header />
     </div>
   );
 }
