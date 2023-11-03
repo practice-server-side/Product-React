46cfab4dd16f6cd4b51a93fbb25d0534523c3c94
Roy <yyss9103@gmail.com>
2023-11-03 23:28:33 +0900

    myPage API 연동

--- src/api/cust/GetMyInfo.ts
+++ src/api/cust/GetMyInfo.ts
@@ -1,1 +1,11 @@
-export type GetMyInfo = {};
+import kotlinClient from "../KotlinClient";
+
+export type GetMyInfo = {
+  custName: string;
+  custPhone: string;
+  custKey: string;
+};
+
+export const getMyInfo = () => {
+  return kotlinClient.get<GetMyInfo>("/api/ch/cust");
+};
