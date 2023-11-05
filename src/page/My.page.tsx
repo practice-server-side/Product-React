5c6526741eaae92edbcff48cd52314b385ebc385
Roy <yyss9103@gmail.com>
2023-11-05 19:38:19 +0900

    내정보 출력 (뭔가 좀 구조가 이상하긴함)

--- src/page/My.page.tsx
+++ src/page/My.page.tsx
@@ -4,11 +4,15 @@ import myInfoHook from "../hook/MyInfoHook";
 export const MyPage = () => {
   const { myInfoData } = myInfoHook();
 
-  console.log("myInfoData.data", myInfoData.data);
-
   return (
     <>
       <h1>내 정보</h1>
+      {myInfoData.data?.custName}
+      <br />
+      {myInfoData.data?.custPhone}
+      <br />
+      {myInfoData.data?.custKey}
+      <br />
     </>
   );
 };
