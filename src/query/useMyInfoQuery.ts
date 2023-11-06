09b85810a662715305c27ffd071bd511a2b579e4
Author Name <yyss9103@gmail.com>
2023-11-06 23:30:43 +0900

    성공했음에도 계속 요청보내는거 비활성화

--- src/query/useMyInfoQuery.ts
+++ src/query/useMyInfoQuery.ts
@@ -3,6 +3,9 @@ import { useQuery } from "react-query";
 
 export const useMyInfoQuery = () => {
   return useQuery("/api/ch/cust", getMyInfo, {
+    refetchOnWindowFocus: false, // 윈도우 초점을 받았을 때 새로고침 비활성화
+    refetchOnReconnect: false, // 네트워크 재연결 시 새로고침 비활성화
+    refetchOnMount: false, // 마운트 시 새로고침 비활성화
     retry: false,
     onError: (error) => {
       console.log("에러 발생", error);
