18b574750f3aa4b6394b8992d5434b1c2771911e
Roy <yyss9103@gmail.com>
2023-11-09 21:22:04 +0900

    몰리스트 페이지 만들기

--- src/page/My.page.tsx
+++ src/page/My.page.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import myInfoHook from "../hook/MyInfoHook";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const MyPage = () => {
   const { data } = myInfoHook();
+  const [menu] = useState([{ label: "마이 몰 리스트", path: "/myPage/mall" }]);
 
   return (
     <>
@@ -13,6 +16,8 @@ export const MyPage = () => {
       <br />
       {data?.custKey}
       <br />
+      <tr />
+      <Link to={menu[0].path}>{menu[0].label}</Link>
     </>
   );
 };
---
+++ src/page/MyMallList.page.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const MyMallList = () => {
+  return <>몰리스트</>;
+};
+
+export default MyMallList;
